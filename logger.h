3d57d4aa12f86b9ede83e41f4fcf438aadb51ad5
zixun <lvzixun@gmail.com>
2016-07-13 14:13:38 +0800

    fix LOGV

--- logger.h
+++ logger.h
@@ -17,7 +17,7 @@ extern "C"
 #define LOGI(...)		__android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGE(...)		__android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 #define LOGW(...)		__android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
-#define LOGV(fmt, ap)	__android_log_print(ANDROID_LOG_INFO, LOG_TAG, (fmt), (ap))
+#define LOGV(fmt, ap)	__android_log_vprint(ANDROID_LOG_INFO, LOG_TAG, (fmt), (ap))
 
 #else
 
